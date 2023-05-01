2e067fec36287c17b5a735fe8482f01a01866c13
chang <changliangliang1996@foxmail.com>
2023-05-02 00:59:53 +0800

    vault backup: 2023-05-02 00:59:53
    Affected files:
    .obsidian/plugins/quickadd/data.json
    scrpit/da.js
    看板/草稿管理.md

--- scrpit/da.js
+++ scrpit/da.js
@@ -1,6 +1,43 @@
-module.exports = async (params) => {
-    const { quickAddApi: { inputPrompt, suggester } } = params;
-    console.log(params)
-    const val = await inputPrompt("📖 Book Name");
+const TEXT_FIELD = "Text field";
+module.exports = {
+    entry: async (QuickAdd, settings) => {
 
+        const {
+            quickAddApi: { inputPrompt },
+        } = QuickAdd;
+
+        console.log(QuickAdd)
+
+        const filename = await inputPrompt("新建草稿", "草稿名称")
+    },
+    settings: {
+        name: "Demo",
+        author: "Christian B. B. Houmann",
+        options: {
+            [TEXT_FIELD]: {
+                type: "text",
+                defaultValue: "",
+                placeholder: "Placeholder",
+            },
+            "Checkbox": {
+                type: "checkbox",
+                defaultValue: false,
+            },
+            "Dropdown": {
+                type: "dropdown",
+                defaultValue: "Option 1",
+                options: [
+                    "Option 1",
+                    "Option 2",
+                    "Option 3",
+                ],
+            },
+            "Format": {
+                type: "format",
+                defaultValue: "{{DATE:YYYY-MM-DD}}",
+                placeholder: "Placeholder",
+            },
+        }
+    },
 }
+
